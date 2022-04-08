b5c92c93b5dd3fb5506ff15ffdaffd01d947b00f
sagar chandankhede <sagarchandankhede09@gmail.com>
2022-04-09 00:59:57 +0530

    5th new time commit

--- src/abstractClass/Concrete.java
+++ src/abstractClass/Concrete.java
@@ -21,5 +21,6 @@ public class Concrete extends Sample {
 	public void test1() {
 		System.out.println("Method from concrete class");
 		System.out.println("this is new added line for git study");
+		System.out.println("adding one more line for github");
 	}
 }
